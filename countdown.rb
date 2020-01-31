cd2546f5ec478e31f98082fd5251e06d9496fbab
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-31 23:44:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,7 @@
-#write your code here
 def countdown_to_the_midnight(num)
-  count = 10 
-  while count > 0 do 
-    puts puts "#{count} SECOND(S)!"
-    count -= 1 
+  while num > 0 do 
+    puts puts "#{num} SECOND(S)!"
+    num -= 1 
   end
   return "HAPPY NEW YEAR!"
 end
