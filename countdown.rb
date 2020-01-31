81e4b5bc2d023154fc59b3af4c2e5db9b9fcca09
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-31 23:26:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,2 @@
 #write your code here
 
-def countdown(number)
-  newstr = ""
-  i = 0 
-  while i < number.length do 
-    newstr+= "#{number[i]} SECOND(S)! HAPPY NEW YEAR!"
-    i += 1 
-  end
-  return newstr
-end
